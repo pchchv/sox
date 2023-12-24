0a8036b75e7fde2fcb7e09e5e3280a0df0bd69a6
pchchv <ipchchv@gmail.com>
2023-12-24 13:40:46 +0300

    Implement a splitHostPort function

---
+++ net.go
@@ -0,0 +1,19 @@
+package sox
+
+import (
+	"net"
+	"strconv"
+)
+
+func splitHostPort(addr string) (host string, port uint16, err error) {
+	host, portStr, err := net.SplitHostPort(addr)
+	if err != nil {
+		return "", 0, err
+	}
+	portInt, err := strconv.ParseUint(portStr, 10, 16)
+	if err != nil {
+		return "", 0, err
+	}
+	port = uint16(portInt)
+	return
+}
