468b3a1bf54dd67ac48c112edc8a54a9e6c96166
pchchv <ipchchv@gmail.com>
2023-12-24 13:36:24 +0300

    Implement a dialError function

--- socks.go
+++ socks.go
@@ -1,8 +1,16 @@
 package socks
 
+import "net"
+
 // Constants to choose which version of SOCKS protocol to use.
 const (
 	SOCKS4 = iota
 	SOCKS4A
 	SOCKS5
 )
+
+func dialError(err error) func(string, string) (net.Conn, error) {
+	return func(_, _ string) (net.Conn, error) {
+		return nil, err
+	}
+}
