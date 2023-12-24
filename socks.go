7356b36e8a64477d6ca3d6cd6081962db4c2a67c
pchchv <ipchchv@gmail.com>
2023-12-24 13:52:04 +0300

    Implement a dialFunc method

--- socks.go
+++ socks.go
@@ -1,6 +1,9 @@
 package sox
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 // Constants to choose which version of SOCKS protocol to use.
 const (
@@ -8,6 +11,20 @@ const (
 	SOCKS4A
 	SOCKS5
 )
+
+func (cfg *config) dialFunc() func(string, string) (net.Conn, error) {
+	switch cfg.Proto {
+	case SOCKS5:
+		return func(_, targetAddr string) (conn net.Conn, err error) {
+			return cfg.dialSocks5(targetAddr)
+		}
+	case SOCKS4, SOCKS4A:
+		return func(_, targetAddr string) (conn net.Conn, err error) {
+			return cfg.dialSocks4(targetAddr)
+		}
+	}
+	return dialError(fmt.Errorf("unknown SOCKS protocol %v", cfg.Proto))
+}
 
 func dialError(err error) func(string, string) (net.Conn, error) {
 	return func(_, _ string) (net.Conn, error) {
