b6f5938f3fe2db3e98cff389c7e1e5de52daa68b
pchchv <ipchchv@gmail.com>
2023-12-24 13:27:25 +0300

    Add a config type

--- parse.go
+++ parse.go
@@ -1,6 +1,15 @@
 package socks
 
+import "time"
+
 type auth struct {
 	Username string
 	Password string
+}
+
+type config struct {
+	Proto   int
+	Host    string
+	Auth    *auth
+	Timeout time.Duration
 }
