9eabc392a0326fc86069b3e8c7910d47b136e2e3
pchchv <ipchchv@gmail.com>
2023-12-24 13:42:30 +0300

    Implement a lookupIPv4 function

--- net.go
+++ net.go
@@ -1,6 +1,7 @@
 package sox
 
 import (
+	"errors"
 	"net"
 	"strconv"
 )
@@ -10,10 +11,28 @@ func splitHostPort(addr string) (host string, port uint16, err error) {
 	if err != nil {
 		return "", 0, err
 	}
+
 	portInt, err := strconv.ParseUint(portStr, 10, 16)
 	if err != nil {
 		return "", 0, err
 	}
+
 	port = uint16(portInt)
 	return
+}
+
+func lookupIPv4(host string) (net.IP, error) {
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ip := range ips {
+		ipv4 := ip.To4()
+		if ipv4 == nil {
+			continue
+		}
+		return ipv4, nil
+	}
+	return nil, errors.New("no IPv4 address found for host: " + host)
 }
